d6d3a93994c3aa70192505be2385beb273bbc860
Alparslan Kilinc <kilincalparslan70@gmail.com>
2022-05-08 17:23:29 -0400

    Game logic Done

--- script.js
+++ script.js
@@ -4,33 +4,28 @@ let UserWins=0;
 let ComputerWins=0; 
 
 let res = document.querySelector('.result');
+let C_Score= document.querySelector('.ComputerWins');
+let P_Score= document.querySelector('.PlayerWins');
+
+
 
 const buttons = document.querySelectorAll('button');
     buttons.forEach((button)=>{
     button.addEventListener('click',()=>{
-       let UW =UserWins;
-       let CW =ComputerWins;
-       let input= button.className;
-       if(UW<5 || CW<5){
-        let scores=Game(input,res);
-       }else{
-        if(UW>CW)
-        {
-         res.textContent='Game Over,You Won';
-        }
-     else{
-         res.textContent='Game Over,You Lost';
-         }
-       }
+    let UW =UserWins;
+    let CW =ComputerWins;
+    let input= button.className;
+   
+    if(UW<5 && CW<5){
+        userPlay(input);
+        Game(input,res);
+    } 
+    
     });
 });
 
 
-/// Functions
-
-
    
- 
 
 function Game(input,res){ 
   
@@ -39,20 +34,23 @@ function Game(input,res){
     
             if(result==1){
                 UserWins++;
+            P_Score.textContent=`Player Score: ${UserWins}`;
                 res.textContent='You Win';
             }else if(result==-1){
                 ComputerWins++;
                 res.textContent='You Lose';
+                C_Score.textContent=`Computer Score: ${ComputerWins}`;
             }else{
              res.textContent='Tie';
             }
+            if(ComputerWins>=5 || UserWins>=5) checkResult();
         
            
 }
 
 function computerPlay(){
         let randomNumber= Math.round(Math.random()*2);
-        let computer_P = document.getElementsByClassName('comp-pic');
+        let computer_P = document.querySelector('#C_img');
         if(randomNumber==0){
             computer_P.src='/Images/Rock.png';
             console.log('Computer Play: paper');
@@ -92,7 +90,41 @@ function PlayRound(computerSelection, playerSelection){
         }
     }
 
+function userPlay(selection){
+    let U_choice = document.querySelector('#U_pic');
+    switch(selection){
+        case 'rock':
+            U_choice.src='/Images/Rock.png';
+            break;
+        case 'paper':
+            U_choice.src='/Images/Paper.png';
+            break;
+        case 'scissors':
+            U_choice.src='/Images/Scissors.png';
+            break;
 
+    }
+   
 
+}
+
+function checkResult(){
+    if(UserWins>ComputerWins)
+{
+res.textContent='Game Over,You Won';
+document.getElementById('#endScreen').style.display='flex';
+ 
+}
+else if(ComputerWins>UserWins){
+res.textContent='Game Over,You Lost';
+document.getElementById('#endScreen').style.display='flex';
+ 
+ }
+
+}
+
+function replay(){
+
+}
 
 
